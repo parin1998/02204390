acdea2a4c7e6d70c7993564c27c36ad1fa09ec33
parin1998 <60882177+parin1998@users.noreply.github.com>
2020-11-02 21:21:35 +0700

    test 2

---
+++ header.php
@@ -0,0 +1,12 @@
+
+    <div class="container">
+        <a href="index.php" id="logo" title="Diana’s jewelry">Diana’s jewelry</a>
+        <div class="right-links">
+            <ul>
+                <li><a href="cart.html"><span class="ico-products"></span>3 products, $4 500.00</a></li>
+                <li><a href="#"><span class="ico-account"></span>Account</a></li>
+                <li><a href="#"><span class="ico-signout"></span>Sign out</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- / container -->
